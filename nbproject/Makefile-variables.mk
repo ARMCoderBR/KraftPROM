49521423ee3cacf5b8442177f2e00ad2ef4b86b6
Milton Maldonado Jr. <milton@cpstecnologia.com.br>
2025-07-10 11:37:11 -0300

    Update Hex file.

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -6,5 +6,5 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=EEPROM_Prog.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/EEPROM_Prog.production.hex
+CND_ARTIFACT_NAME_default=KraftPROM.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/KraftPROM.production.hex
